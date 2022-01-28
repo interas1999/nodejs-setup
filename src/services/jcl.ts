a45e441f4c047d384fce3f3ef51ab8a320d2104d
MarcusSilva3298 <marcussilva3298@gmail.com>
2022-01-28 16:37:32 -0300

    Revert "tests"
    This reverts commit 4814183e309b1d000a003c976812bc219b1d2853.
    
    cancel commit

--- src/services/jcl.ts
+++
@@ -1,5 +0,0 @@
-import axios from 'axios'
-
-export const jcl = axios.create({
-  baseURL: 'http://138.204.221.94:6002/api'
-})
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,18 +1,7 @@
-import { JclRepository } from '@modules/JCL/infra/repositories/JclRepository'
 import { Router } from 'express'
 
 const routes = Router()
 
-routes.use('/', async (req, res) => {
-  const jclRepo = new JclRepository()
-
-  await jclRepo.getToken()
-
-  const devedor = await jclRepo.findDevedorByCPF('00090458559415')
-  const answer = await jclRepo.getBoletos(devedor[0].CPF)
-
-  console.log(answer)
-  res.json('Hello World')
-})
+routes.use('/', (req, res) => { res.send('Hello World') })
 
 export { routes }
